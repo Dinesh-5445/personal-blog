2eafc6dada7cffa4b4ffe0b025f2429740e77434
Dinesh <dinesh.palavalasa2007@gmail.com>
2026-07-21 23:23:08 +0530

    Add second blog post

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,10 @@ export default function Footer() {
     <footer className="container">
       <p>© {new Date().getFullYear()} Dinesh Palavalasa.</p>
       <div className="flex gap-6">
-        <a href="https://github.com/Dinesh-Palavalasa" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/Dinesh-5445" target="_blank" rel="noopener noreferrer">
           GitHub
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/dinesh-palavalasa-a53250389" target="_blank" rel="noopener noreferrer">
           LinkedIn
         </a>
       </div>
